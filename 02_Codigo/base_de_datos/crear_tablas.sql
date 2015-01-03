8704edd8488818a68d45d1e4bc2f9b87adc53bcb
Richard Castro <richard.tk.13@gmail.com>
2015-01-03 16:37:33 -0500

    Actualizando el script de la base de datos.

--- 02_Codigo/base_de_datos/crear_tablas.sql
+++ 02_Codigo/base_de_datos/crear_tablas.sql
@@ -17,3 +17,14 @@ CREATE TABLE `TBiblioTecaVirtual` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=0;
+
+DROP TABLE IF EXISTS `TOpcion`;
+CREATE TABLE `TOpcion` (
+	`ID` INT(11) NOT NULL AUTO_INCREMENT,
+	`Codigo` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8_general_ci',
+	`Descripcion` VARCHAR(1000) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`ID`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
