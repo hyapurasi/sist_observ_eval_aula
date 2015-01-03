42986e683fff09074a1ef2615e050ff84091285e
Richard Castro <richard.tk.13@gmail.com>
2015-01-03 16:25:14 -0500

    Actualizando el script de la base de datos.

--- 02_Codigo/base_de_datos/crear_tablas.sql
+++ 02_Codigo/base_de_datos/crear_tablas.sql
@@ -7,4 +7,13 @@ CREATE DATABASE `bdQhawaqIrqi`
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-
+DROP TABLE IF EXISTS `TBiblioTecaVirtual`;
+CREATE TABLE `TBiblioTecaVirtual` (
+	`ID` INT(11) NOT NULL AUTO_INCREMENT,
+	`Code` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8_general_ci',
+	`Description` VARCHAR(1000) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`ID`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
