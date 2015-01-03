955138ef7ae3362fb413ac1c3c9e61902672564d
Richard Castro <richard.tk.13@gmail.com>
2015-01-03 15:16:56 -0500

    Establecer el nombre de la base de datos y la codificación utf8 que se utilizará en el sistema.

--- 02_Codigo/base_de_datos/crear_tablas.sql
+++ 02_Codigo/base_de_datos/crear_tablas.sql
@@ -1,2 +1,10 @@
+-- Gestor de base de datos: MySQL
 -- Creando tablas
 -- Base de datos: bdQhawaqIrqi (Niño Observador)
+
+DROP DATABASE IF EXISTS `bdQhawaqIrqi`;
+CREATE DATABASE `bdQhawaqIrqi` 
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+
